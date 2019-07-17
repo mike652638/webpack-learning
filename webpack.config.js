9eb3c98db9da74b6a7660ea6f5a8296624f382bf
mike652638 <info@mike652638.com>
2019-07-17 17:26:42 +0800

    webpack loaders & plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,27 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+/* //以上为ES6解构赋值写法, 相当于
+const _CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = _CleanWebpackPlugin.CleanWebpackPlugin;
+//参考: https://stackoverflow.com/questions/41058569/what-is-the-difference-between-const-and-const-in-javascript */
+
+
 module.exports = {
   //mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js'
+  },
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, './dist')
   },
   module: {
+    //Webpack loaders对模块进行预处理,
+    //比如对于scss样式文件, 通过sass- loader进行编译成为css文件, 再通过postcss- loader对某些CSS3样式添加浏览器厂商前缀, 再经过css-loader对模块化css文件进行解析引入, 最终通过style-loader将解析后的css注入到head->style标签内
     rules: [{
         test: /\.(jpg|jpeg|png|gif)$/,
         use: {
@@ -49,7 +62,9 @@ module.exports = {
       }
     ]
   },
+  //webpack plugins类似Vue中的生命周期钩子函数, 在特定的时刻执行某个操作, 
+  //例如下方html- webpack - plugin就是在打包过程完成后, 以指定或默认的template为模板, 生成一个HTML文件, 并将打包的文件注入到生成的HTML中;
   plugins: [new HtmlWebpackPlugin({
     template: './src/index.html'
-  })]
+  }), new CleanWebpackPlugin()]
 }
