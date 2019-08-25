f53dac24806cf51036c3fac6a4f1a14b13229b0b
mike652638 <info@mike652638.com>
2019-08-25 11:12:03 +0800

    ..

--- src/content.js
+++ src/content.js
@@ -2,7 +2,7 @@ function Content() {
   let content = document.createElement('div');
   let rootEle = document.getElementById('root');
   content.id = 'content';
-  content.innerText = 'This is Content ... !!!';
+  content.innerText = 'This is Content ... ';
   rootEle.append(content);
 }
 
